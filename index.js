900505257717e499008ee1c63cb3b62fee279040
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    "Initial commit"

---
+++ index.js
@@ -0,0 +1,6 @@
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+
+process.env.OPENAI_API_KEY = "2131321";
+console.log(process.env.OPENAI_API_KEY);
+const chat = new ChatOpenAI({ temperature: 0 });
