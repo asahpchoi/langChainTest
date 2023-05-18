a9cf591950805c1f69567297e1daff2cbff49baa
Asa Choi <asa.choi@gmail.com>
2023-05-18 16:52:39 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { askGPT, loadPDF } from "./langChainHelper.js";
 const directory = "idx/3";
 const file = "Handbook_0700_17_Aug_2021.pdf";
+import express from "express";
+import cors from "cors";
 
 const init = async () => {
   //const load = await loadPDF(file, directory);
 
-  let question = "my name is asa choi";
+  let question = "Summary the docs";
   let history = "";
   let reply = await askGPT(question, directory, history);
 
@@ -18,4 +20,14 @@ const init = async () => {
   console.log({ reply });
 };
 
-init();
+var app = express();
+
+app.use(cors());
+
+app.get("/", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(3000, function () {
+  console.log("CORS-enabled web server listening on port 80");
+});
